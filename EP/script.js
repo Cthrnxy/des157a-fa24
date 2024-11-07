af9b543b0767cd84cd3438092aee8ce52ef60640
Catherine Y <catheriney@campus-056-180.ucdavis.edu>
2024-11-07 13:33:25 -0800

    add new things

---
+++ EP/script.js
@@ -0,0 +1,24 @@
+(function(){
+    "use strict"
+    console.log('reading JS');
+    var slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+setInterval(function() {changeSlide(1);}, 3000); 
+})()
